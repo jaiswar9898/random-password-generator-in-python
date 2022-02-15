c31796cc69e96f1d5855a530c9b33e4e0dfaec57
Jaiswar8888 <87074705+Jaiswar8888@users.noreply.github.com>
2022-02-15 17:23:58 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import random 
+
+chars= 'abcefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWZYZ123456'
+
+while 1:
+   password_len = int(input("What length would you like your password to be :"))
+   password_count = int(input("How many password would you like "))
+   for x in range(0,password_count):
+          password = ""
+          for x in range(0,password_len):
+               password_char = random.choice(chars)
+               password = password + password_char
+          print("Here is your password : ",password)
+                
+
+
