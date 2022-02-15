7d699ef9d71125424bd09bb21bcb42a8f16ce9e9
Jaiswar8888 <87074705+Jaiswar8888@users.noreply.github.com>
2022-02-15 18:57:28 +0530

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,19 @@
+from multiprocessing.spawn import import_main_path
+
+
+import random 
+from random import randint 
+password = ""
+for i in range(10):
+   i = chr(random.randint(65,90))
+   password = str(password) + i 
+print(password)
+
+#Upper and lowercase password 
+password = ""
+for i in range(5):
+   i = chr(randint(65,90))
+   for j in range(5):
+      j = chr(randint(65,90)).lower()
+   password = str(password) + i + j
+print(password)
